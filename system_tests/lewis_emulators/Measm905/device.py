683a76c56e295494821870451a4283067cfbdfc0
esmithExperimentControls <evan.smith@stfc.ac.uk>
2023-07-11 16:56:23 +0100

    wrote method to read pressure from IOC

--- system_tests/lewis_emulators/Measm905/device.py
+++ system_tests/lewis_emulators/Measm905/device.py
@@ -1,16 +1,18 @@
 from collections import OrderedDict
 from .states import DefaultState
 from lewis.devices import StateMachineDevice
+import random
+
 
 
 class SimulatedMeasm905(StateMachineDevice):
-    pressure = 9
+    
 
     def _initialize_data(self):
         """
         Initialize all of the device's attributes.
         """
-        pass
+        self._pressure = 1
 
     def _get_state_handlers(self):
         return {
@@ -23,4 +25,8 @@ class SimulatedMeasm905(StateMachineDevice):
     def _get_transition_handlers(self):
         return OrderedDict([
         ])
-
+    
+    @property
+    def pressure(self):
+        return self._pressure
+    
